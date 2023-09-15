19fb36873326c3e1e4c683e71dca8862dd12e8cb
YUT4K4 <admin@openpredict.org>
2023-09-15 19:47:02 +0000

    try again

--- packages/backend/amclient/index.ts
+++ packages/backend/amclient/index.ts
@@ -65,7 +65,7 @@ export async function getHelia() {
         datastore: globalThis.heliaDatastore!,
         // libp2p: globalThis.libp2p
       })
-      // globalThis._helia.libp2p.services.dht.setMode("server");
+      globalThis._helia.libp2p.services.dht.setMode("server");
       globalThis.helia = hJson(globalThis._helia);
     }
   })
