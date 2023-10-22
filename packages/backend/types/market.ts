4042a88fc0330b47e9cf88b4364b777c82c7781b
YUT4K4 <admin@openpredict.org>
2023-10-21 22:04:43 -0400

    wip

--- packages/backend/types/market.ts
+++ packages/backend/types/market.ts
@@ -10,7 +10,7 @@ export const searchMarketsSchemaV0 = z.object({
   term: z.string().optional(),
   skip: z.number().optional(),
   limit: z.number().optional(),
-  orderBy: z.enum(["recent", "volume"]),
+  orderBy: z.enum(["recent", "volume"]).optional(),
 })
 
 export const getMarketSchemaV0 = z.object({
@@ -190,8 +190,6 @@ export type marketFulldata = {
 
 export type extMarketChaindata = {
   data: marketChaindata,
-  CommentCount: number,
-  Likes: Set<string>,
   UserAccounts: Map<string, marketUserChaindata>
   PriceHistory: marketPricePoint[]
   Trades: marketTradeChaindata[]
