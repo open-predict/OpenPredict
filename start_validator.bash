9babae11020572533271bb5a3eaa9daf9b37c0a5
YUT4K4 <admin@openpredict.org>
2023-11-07 01:11:02 -0500

    Fix start_validator.bash

--- start_validator.bash
+++ start_validator.bash
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -m
+mkdir -p ./test-accounts
 
 #Start validator
 solana-test-validator --account-dir ./test-accounts >/dev/null &
 TEST_VALIDATOR_PID=$!
 function clean_up {
     # Perform program exit housekeeping
-    KILL $TEST_VALIDATOR_PID
+    kill $TEST_VALIDATOR_PID
     exit
 }
 trap clean_up SIGHUP SIGINT SIGTERM
@@ -20,7 +21,7 @@ FEE_PAYER_PUBKEY="$(solana-keygen pubkey ./fee_payer.json)"
 
 # Create mint address if not exists
 if [[ -f ./test-accounts/usdc_mint.json ]]; then
-  USDC_MINT_AUTH_ADDR=$(jq .pubkey ./test-accounts/usdc_mint.json)
+  USDC_MINT_AUTH_ADDR=$(jq -r .pubkey ./test-accounts/usdc_mint.json)
 else
   USDC_MINT_AUTH_ADDR=$(spl-token create-token --output json -u localhost | jq .commandOutput.address -r)
   solana account $USDC_MINT_AUTH_ADDR --output-file ./test-accounts/usdc_mint.json --output json -u localhost
@@ -30,9 +31,12 @@ fi
 cd ./packages/backend/contracts/
 RUST_BACKTRACE=1 USDC_MINT_AUTH_ADDR=$USDC_MINT_AUTH_ADDR cargo build-sbf
 [ -f ./target/deploy/openpredict-keypair.json ] || solana-keygen new --no-bip39-passphrase -o ./target/deploy/openpredict-keypair.json
+deployOutput=$(solana program deploy ./target/deploy/openpredict.so -u localhost --output json)
+MAIN_PROGRAM_ID=$(echo $deployOutput | jq -r '.programId')
 cd ../../../
 
 echo "Fee payer key: $FEE_PAYER_PUBKEY"
 echo "Mint address: $USDC_MINT_AUTH_ADDR"
+echo "Main program id: $MAIN_PROGRAM_ID"
 echo "Validator running and active!"
 wait $TEST_VALIDATOR_PID
